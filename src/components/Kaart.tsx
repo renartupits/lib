ac9f0b5c2265e07a9cfcfd50bd9617527f1d7b69
Renar Tupits <renar.tupits@kwota.com>
2025-01-07 14:39:54 +0200

    add tailwindcss eslint

--- src/components/Kaart.tsx
+++ src/components/Kaart.tsx
@@ -1,4 +1,3 @@
-// src/Kaart.tsx
 import React from 'react';
 
 interface ButtonProps {
@@ -9,7 +8,7 @@ interface ButtonProps {
 export const Kaart: React.FC<ButtonProps> = ({ children, onClick }) => {
   return (
     <button
-      className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
+      className="rounded bg-blue-500 px-4 py-20 text-white hover:bg-blue-600"
       onClick={onClick}
     >
       {children}
